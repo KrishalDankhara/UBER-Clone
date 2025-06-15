26788c005f3e84cbbecb24ddd60db0c9ca2f3e6a
Krishal Dankhara <dankharakrishal2005@gmail.com>
2025-06-16 00:40:16 +0530

    user authentication done

--- Uber/Backend/app.js
+++ Uber/Backend/app.js
@@ -3,6 +3,7 @@ dotenv.config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const cookieParser = require('cookie-parser');
 const connectToDb = require('./db/db');
 connectToDb();
 const userRoutes = require('./routes/user.routes');
@@ -10,8 +11,7 @@ const userRoutes = require('./routes/user.routes');
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-// app.use(cookieParser());
+app.use(cookieParser());
 
 app.get('/', (req, res) => {
     res.send('Hello World');
---
+++ Uber/Backend/middlewares/auth.middleware.js
@@ -0,0 +1,35 @@
+//middleware is used to check if token is valid or not, if it is not valid then it is unauthorized
+// if it is valid then it middleware decodes the token and if error occur then it is also unauthorized
+
+const userModel = require('../models/user.model');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const blackListTokenModel = require('../models/blackListToken.model');
+
+module.exports.authUser = async (req, res, next) => {
+    const token = req.cookies.token || req.headers.authorization?.split(' ')[ 1 ];
+
+    if (!token) {
+        return res.status(401).json({ message: 'Unauthorized' });
+    }
+
+
+    const isBlacklisted = await blackListTokenModel.findOne({ token: token });
+
+    if (isBlacklisted) {
+        return res.status(401).json({ message: 'Unauthorized' });
+    }
+
+    try {
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const user = await userModel.findById(decoded._id)
+
+        req.user = user;
+
+        return next();
+
+    } catch (err) {
+        return res.status(401).json({ message: 'Unauthorized' });
+    }
+}
