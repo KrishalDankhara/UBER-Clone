bb0de79f1d7ca3e701b86c36f437c69be3334053
Krishal Dankhara <dankharakrishal2005@gmail.com>
2025-06-15 23:04:18 +0530

    user registration done

---
+++ Uber/Backend/routes/user.routes.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const router = express.Router();
+const { body } = require("express-validator") //used for validate user details
+const userController = require('../controllers/user.controller');
+// const authMiddleware = require('../middlewares/auth.middleware');
+
+
+router.post('/register', [  //using expression validator
+    body('email').isEmail().withMessage('Invalid Email'),
+    body('fullname.firstname').isLength({ min: 3 }).withMessage('First name must be at least 3 characters long'),
+    body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long')
+],
+    userController.registerUser //redirect to user.controller.js file for user registration logic
+)
+
+// router.post('/login', [
+//     body('email').isEmail().withMessage('Invalid Email'),
+//     body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long')
+// ],
+//     userController.loginUser
+// )
+
+// router.get('/profile', authMiddleware.authUser, userController.getUserProfile)
+
+// router.get('/logout', authMiddleware.authUser, userController.logoutUser)
+
+
+
+module.exports = router;
---
+++ Uber/Backend/services/user.service.js
@@ -0,0 +1,20 @@
+const userModel = require('../models/user.model');
+
+// check user data if matching all requirnments then return user details
+module.exports.createUser = async ({
+    firstname, lastname, email, password
+}) => {
+    if (!firstname || !email || !password) {
+        throw new Error('All fields are required');
+    }
+    const user = userModel.create({
+        fullname: {
+            firstname,
+            lastname
+        },
+        email,
+        password
+    })
+
+    return user;
+}
